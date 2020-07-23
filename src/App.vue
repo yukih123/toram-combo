4188a21e160cca298ce5aa1f6a3a70cd44f31a87
yukih123 <hiromi.yuki1@gmail.com>
2020-07-23 11:19:56 +0900

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
     <div id="app">
-        <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <Calc/>
     </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calc from './components/Calc.vue'
 
 export default {
     name: 'App',
     components: {
-        HelloWorld
+        Calc
     }
 }
 </script>
