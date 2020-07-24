90594469bf505f3044f89899adc9a2e22985aff7
yukih <hogepiyo774@gmail.com>
2020-07-24 17:51:25 +0900

    Adjust layout and style

--- src/App.vue
+++ src/App.vue
@@ -20,8 +20,11 @@ export default {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+}
+body {
+    margin: 0;
+    color: #353866;
+    background-color: #F8FAFF;
+    border-bottom: 15px #CBA6C3 solid;
 }
 </style>
