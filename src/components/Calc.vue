d3df3978572af992fba76af8eee2ec6a8fc97970
yuki <hogepiyo774@gmail.com>
2020-07-24 17:51:25 +0900

    Adjust layout and style

--- src/components/Calc.vue
+++ src/components/Calc.vue
@@ -1,61 +1,81 @@
 <template>
-    <div>
-        <h1>トーラム コンボ計算</h1>
-        <el-row v-for="(row, row_index) in rows" :key="row_index" :gutter="10">
-            <el-col :span="16">
-                <el-select
-                    placeholder="スキルを選択"
-                    v-model="row.skill"
-                    value-key="name"
-                    :disabled="isDisabledSkill(row_index)"
-                    @change="change()">
-                    <el-option-group
-                        v-for="(category, category_index) in skill_categories"
-                        :key="category_index"
-                        :label="category.name">
-                        <el-option
-                            v-for="(skill, skill_index) in category.skills"
-                            :key="skill_index"
-                            :label="skill.name + ' ' + skill.mp"
-                            :value="{
-                                name: skill.name,
-                                mp: skill.mp,
-                                halve_next: skill.halve_next,
-                            }">
-                        </el-option>
-                    </el-option-group>
-                </el-select>
-            </el-col>
-            <el-col :span="6">
-                <el-select
-                    v-if="row_index != 0"
-                    v-model="row.effect"
-                    placeholder="特殊効果を選択"
-                    :disabled="isDisabledSkill(row_index)">
-                    <el-option
-                        v-for="(effect, key) in effects"
-                        :key="key"
-                        :label="effect"
-                        :value="key"
-                        :disabled="isDisabledEffect(row_index) && key != 'none'">
-                    </el-option>
-                </el-select>
-                <div v-else>なし</div>
-            </el-col>
-            <el-col :span="2">
-                <i v-if="row.skill.name != null" class="el-icon-delete" @click="deleteRow(row_index)"></i>
-            </el-col>
-        </el-row>
-        <el-row :gutter="10">
-            消費MP {{ result }}
-        </el-row>
-        <el-row :gutter="10">
-            コンボポイント {{ point }}
-        </el-row>
-    </div>
+    <el-container>
+        <el-header height="auto">
+            <h1>{{ config.title }}</h1>
+        </el-header>
+        <el-main>
+            <div class="tip">
+                <p>コンボのMPを計算するツールです。</p>
+                <p>スキルと特殊効果を順に選んでください。</p>
+            </div>
+            <div class="form">
+                <el-row :gutter="10" class="form_title">
+                    <el-col :span="16">スキル</el-col>
+                    <el-col :span="6">特殊効果</el-col>
+                </el-row>
+                <el-row v-for="(row, row_index) in rows" :key="row_index" :gutter="10">
+                    <el-col :span="16">
+                        <el-select
+                            placeholder="スキルを選択"
+                            v-model="row.skill"
+                            value-key="name"
+                            :disabled="isDisabledSkill(row_index)"
+                            @change="change()">
+                            <el-option-group
+                                v-for="(category, category_index) in skill_categories"
+                                :key="category_index"
+                                :label="category.name">
+                                <el-option
+                                    v-for="(skill, skill_index) in category.skills"
+                                    :key="skill_index"
+                                    :label="skill.name + ' ' + skill.mp"
+                                    :value="{
+                                        name: skill.name,
+                                        mp: skill.mp,
+                                        halve_next: skill.halve_next,
+                                    }">
+                                </el-option>
+                            </el-option-group>
+                        </el-select>
+                    </el-col>
+                    <el-col :span="6">
+                        <el-select
+                            v-model="row.effect"
+                            placeholder="特殊効果を選択"
+                            :disabled="isDisabledSkill(row_index) || row_index == 0">
+                            <el-option
+                                v-for="(effect, key) in effects"
+                                :key="key"
+                                :label="effect"
+                                :value="key"
+                                :disabled="isDisabledEffect(row_index) && key != 'none'">
+                            </el-option>
+                        </el-select>
+                    </el-col>
+                    <el-col :span="2">
+                        <i v-if="row.skill.name != null" class="el-icon-delete" @click="deleteRow(row_index)"></i>
+                    </el-col>
+                </el-row>
+            </div>
+            <div class="result">
+                <el-row :gutter="10">
+                    消費MP <span class="point">{{ result }}</span>
+                </el-row>
+                <el-row :gutter="10">
+                    コンボポイント <span class="point">{{ point }}</span>
+                </el-row>
+            </div>
+        </el-main>
+        <el-footer height="auto">
+            <h2><i class="el-icon-info"></i> お問い合わせ・バグ報告</h2>
+            <p>Twitter <a :href="'https://twitter.com/' + config.account">@{{ config.account }}</a> までご連絡ください。</p>
+            <p class="license">Favicon by <a href="https://icons8.jp/">icons8</a></p>
+        </el-footer>
+    </el-container>
 </template>
 
 <script>
+import config from '../assets/config.min.json'
 import skill_categories from '../assets/skills.min.json'
 import effects from '../assets/effects.min.json'
 
@@ -63,6 +83,7 @@ export default {
     name: 'Calc',
     data() {
         return {
+            config: config,
             skill_categories: skill_categories,
             effects: effects,
             init_row: {
@@ -148,21 +169,76 @@ export default {
             }
         },
         isDisabledSkill(row_index) {
-            return this.point >= 20 && this.rows[row_index].skill.name == null;
+            return this.point >= 20
+                && this.rows[row_index].skill.name == null;
         },
         isDisabledEffect(row_index) {
-            return this.point >= 20 && this.rows[row_index].effect == null;
+            return this.point >= 20
+                && this.rows[row_index].effect == null;
         },
     }
 }
 </script>
 
 <style scoped lang="scss">
+    h1, h2, p {
+        margin: 0;
+    }
+    a {
+        color: #551a8b;
+    }
+    header {
+        padding: 5px 10px;
+        background-color: #AAABD3;
+        h1 {
+            color: #F8FAFF;
+            font-family: 'DS-kinshichi';
+            letter-spacing: -0.1em;
+            text-align: center;
+            &:before, &:after {
+                content: '';
+                display: inline-block;
+                width: 20px;
+                height: 20px;
+                margin: 0 5px;
+                background-image: url("/favicon.ico");
+                background-size: contain;
+                vertical-align: middle;
+            }
+        }
+    }
+    footer {
+        border-top: 1px #CBA6C3 solid;
+        margin: 0 20px;
+        padding: 20px 0;
+        h2 {
+            font-size: medium;
+        }
+        .license {
+            margin-top: 20px;
+        }
+    }
+    .tip {
+        border-bottom: 1px #CBA6C3 solid;
+        margin-bottom: 20px;
+        padding-bottom: 20px;
+    }
+    .form {
+        .form_title {
+            font-size: small;
+            .el-col {
+                padding-left: 6px !important;
+            }
+        }
+    }
+    .result {
+        margin-top: 20px;
+        padding-left: 5px;
+        .point {
+            margin-left: 5px;
+        }
+    }
     .el-select {
         width: 100%;
-    }
-    h1 {
-        font-family: 'DS-kinshichi';
-        letter-spacing: -0.1em;
     }
 </style>
