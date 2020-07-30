7dbde2874d1f12f2a2346ebc066dd0b58418cc95
yukih <hogepiyo774@gmail.com>
2020-07-30 19:16:16 +0900

    Move header & footer to index.html

--- src/components/Calc.vue
+++ src/components/Calc.vue
@@ -1,105 +1,94 @@
 <template>
-    <div>
-        <header>
-            <h1>{{ env.VUE_APP_TITLE }}</h1>
-        </header>
-        <main>
-            <div class="tip">
-                <p>コンボのMPを計算するツールです。</p>
-                <p>スキルと特殊効果を選んでください。</p>
-            </div>
-            <form class="form">
-                <el-row :gutter="5" class="form_title" type="flex">
-                    <el-col :span="17">スキル</el-col>
-                    <el-col :span="6">特殊効果</el-col>
-                </el-row>
-                <template v-for="(row, row_index) in rows">
-                    <el-row :gutter="5" align="middle" type="flex" :key="'form-input-row-' + row_index" class="form_input_row">
-                        <el-col :span="17">
-                            <el-select
-                                placeholder="スキルを選択"
-                                v-model="row.skill"
-                                value-key="name"
-                                :disabled="isDisabledSkill(row_index)"
-                                @change="change(row_index)">
-                                <el-option-group
-                                    v-for="(category, category_index) in skill_categories"
-                                    :key="category_index"
-                                    :label="category.name">
-                                    <el-option
-                                        v-for="(skill, skill_index) in category.skills"
-                                        :disabled="! skill.can_start && row_index == 0"
-                                        :key="skill_index"
-                                        :label="skill.name + ' ' + skill.mp"
-                                        :value="{
-                                            name: skill.name,
-                                            mp: skill.mp,
-                                            halve_next: skill.halve_next,
-                                        }">
-                                    </el-option>
-                                </el-option-group>
-                            </el-select>
-                        </el-col>
-                        <el-col :span="6">
-                            <el-select
-                                v-if="row_index != 0"
-                                v-model="row.effect"
-                                placeholder="特殊効果を選択"
-                                :disabled="isDisabledSkill(row_index) || row_index == 0">
+    <main>
+        <div class="tip">
+            <p>コンボのMPを計算するツールです。</p>
+            <p>スキルと特殊効果を選んでください。</p>
+        </div>
+        <form class="form">
+            <el-row :gutter="5" class="form_title" type="flex">
+                <el-col :span="17">スキル</el-col>
+                <el-col :span="6">特殊効果</el-col>
+            </el-row>
+            <template v-for="(row, row_index) in rows">
+                <el-row :gutter="5" align="middle" type="flex" :key="'form-input-row-' + row_index" class="form_input_row">
+                    <el-col :span="17">
+                        <el-select
+                            placeholder="スキルを選択"
+                            v-model="row.skill"
+                            value-key="name"
+                            :disabled="isDisabledSkill(row_index)"
+                            @change="change(row_index)">
+                            <el-option-group
+                                v-for="(category, category_index) in skill_categories"
+                                :key="category_index"
+                                :label="category.name">
                                 <el-option
-                                    v-for="(effect, key) in effects"
-                                    :key="key"
-                                    :label="effect"
-                                    :value="key"
-                                    :disabled="isDisabledEffect(row_index) && key != 'none'">
+                                    v-for="(skill, skill_index) in category.skills"
+                                    :disabled="! skill.can_start && row_index == 0"
+                                    :key="skill_index"
+                                    :label="skill.name + ' ' + skill.mp"
+                                    :value="{
+                                        name: skill.name,
+                                        mp: skill.mp,
+                                        halve_next: skill.halve_next,
+                                    }">
                                 </el-option>
-                            </el-select>
-                            <div class="first-effect" v-else>なし</div>
-                        </el-col>
-                        <el-col :span="1">
-                            <i v-if="row.skill.name != null" class="el-icon-delete-solid" @click="deleteRow(row_index)"></i>
-                        </el-col>
-                    </el-row>
-                    <p class="error" v-if="row.duplicate_error" :key="'error-' + row_index">
-                        <i class="el-icon-error"></i> 既に使われています
-                    </p>
-                </template>
-                <ul class="result">
-                    <li>消費MP <span class="point">{{ result }}</span></li>
-                    <li>コンボポイント <span class="point">{{ point }}</span></li>
-                </ul>
-            </form>
+                            </el-option-group>
+                        </el-select>
+                    </el-col>
+                    <el-col :span="6">
+                        <el-select
+                            v-if="row_index != 0"
+                            v-model="row.effect"
+                            placeholder="特殊効果を選択"
+                            :disabled="isDisabledSkill(row_index) || row_index == 0">
+                            <el-option
+                                v-for="(effect, key) in effects"
+                                :key="key"
+                                :label="effect"
+                                :value="key"
+                                :disabled="isDisabledEffect(row_index) && key != 'none'">
+                            </el-option>
+                        </el-select>
+                        <div class="first-effect" v-else>なし</div>
+                    </el-col>
+                    <el-col :span="1">
+                        <i v-if="row.skill.name != null" class="el-icon-delete-solid" @click="deleteRow(row_index)"></i>
+                    </el-col>
+                </el-row>
+                <p class="error" v-if="row.duplicate_error" :key="'error-' + row_index">
+                    <i class="el-icon-error"></i> 既に使われています
+                </p>
+            </template>
+            <ul class="result">
+                <li>消費MP <span class="point">{{ result }}</span></li>
+                <li>コンボポイント <span class="point">{{ point }}</span></li>
+            </ul>
+        </form>
 
-            <details open>
-                <summary><i class="header-icon el-icon-info"></i> 使い方・注意事項</summary>
-                <ul class="details-content">
-                    <li>コンボに組み込みたいスキルと特殊効果を選択してください。選択すると次の入力欄が表示されます。</li>
-                    <li>フィナウ〆などキャンセルで終わらせたい場合は、特殊効果で〆を選んでください。</li>
-                    <li>執念の消費MPは状況によるので、必要MP全てをHPでまかなう(MP消費0)と仮定して計算しています。</li>
-                    <li>全MPを消費するスキルについては調整中です。</li>
+        <details open>
+            <summary><i class="header-icon el-icon-info"></i> 使い方・注意事項</summary>
+            <ul class="details-content">
+                <li>コンボに組み込みたいスキルと特殊効果を選択してください。選択すると次の入力欄が表示されます。</li>
+                <li>フィナウ〆などキャンセルで終わらせたい場合は、特殊効果で〆を選んでください。</li>
+                <li>執念の消費MPは状況によるので、必要MP全てをHPでまかなう(MP消費0)と仮定して計算しています。</li>
+                <li>全MPを消費するスキルについては調整中です。</li>
+            </ul>
+        </details>
+        <details>
+            <summary><i class="header-icon el-icon-question"></i> お問い合わせ・不具合報告</summary>
+            <div class="details-content">
+                <p>Twitter <a :href="'https://twitter.com/' + env.VUE_APP_TWITTER_ACCOUNT">@{{ env.VUE_APP_TWITTER_ACCOUNT }}</a> までご連絡ください。</p>
+                <p>特に下記の情報をいただけると大変助かります。</p>
+                <ul>
+                    <li>起点に使えないスキル</li>
+                    <li>コンボに組み込めないスキル</li>
+                    <li>スキルレベルでMPが変わるスキル</li>
+                    <li>次のスキルMPが半減するスキル</li>
                 </ul>
-            </details>
-            <details>
-                <summary><i class="header-icon el-icon-question"></i> お問い合わせ・不具合報告</summary>
-                <div class="details-content">
-                    <p>Twitter <a :href="'https://twitter.com/' + env.VUE_APP_TWITTER_ACCOUNT">@{{ env.VUE_APP_TWITTER_ACCOUNT }}</a> までご連絡ください。</p>
-                    <p>特に下記の情報をいただけると大変助かります。</p>
-                    <ul>
-                        <li>起点に使えないスキル</li>
-                        <li>コンボに組み込めないスキル</li>
-                        <li>スキルレベルでMPが変わるスキル</li>
-                        <li>次のスキルMPが半減するスキル</li>
-                    </ul>
-                </div>
-            </details>
-        </main>
-        <footer>
-            <p>
-                <a class="tweet" :href="'http://twitter.com/share?url=' + env.VUE_APP_URL + '&text=' + env.VUE_APP_TITLE + '&hashtags=トーラム'" target="_blank">Twitterでシェア</a>
-            </p>
-            <p class="license">Icon by <a href="https://www.deviantart.com/raindropmemory">Raindropmemory</a></p>
-        </footer>
-    </div>
+            </div>
+        </details>
+    </main>
 </template>
 
 <script>
@@ -110,7 +99,6 @@ export default {
     name: 'Calc',
     data() {
         return {
-            env: {},
             skill_categories: skill_categories,
             effects: effects,
             init_row: {
@@ -119,14 +107,15 @@ export default {
                 duplicate_error: false,
             },
             rows: [],
-            active_collapses: ["how_to_use"],
         }
     },
     mounted() {
-        this.env = process.env;
         this.rows.push(Object.assign({}, this.init_row));
     },
     computed: {
+        env() {
+            return process.env;
+        },
         result() {
             const rows = this.rows;
             let sum = 0;
